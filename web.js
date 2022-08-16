959afab6061e2009d5be5d407fcbaa9a76bf9efe
yogiflogic <yogiflogic79@gmail.com>
2022-08-17 00:55:48 +0700

    cloud-run-hackathon
    menambahkan moves2

--- web.js
+++ web.js
@@ -12,6 +12,10 @@ app.post('/', function (req, res) {
   console.log(req.body);
   const moves = ['F', 'T', 'L', 'R'];
   res.send(moves[Math.floor(Math.random() * moves.length)]);
+
+  const moves2 = ['T', 'F', 'R', 'L'];
+  res.send(moves2[Math.floor(Math.random() * moves.length)]);
+
 });
 
 app.listen(process.env.PORT || 8080);
